f47a8911de6e2f882878aa0defcf8b5568b99b24
Allen Baker <althebaker43@gmail.com>
2024-03-02 10:27:16 -0800

    Finished fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,58 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+
+// Clear fill value
+	@0
+	D=A
+	M=D
+
+// Check keyboard
+	@KBD
+	D=M
+	@FILL
+	D;JEQ
+
+// Set fill value
+	D=-1
+	@0
+	M=D
+
+(FILL)
+// Set screen begin and end
+	@8192
+	D=A
+	@SCREEN
+	D=A+D
+	@3
+	M=D
+	@SCREEN
+	D=A
+	@2
+	M=D
+
+(FILL_LOOP)
+// Get next address
+// Set pixels
+	@0
+	D=M
+	@2
+	A=M
+	M=D
+
+// Increment screen pointer
+	D=A+1
+	@2
+	M=D
+
+// Continue fill loop
+	@3
+	D=M-D
+	@FILL_LOOP
+	D;JGT
+
+// Continue main loop
+	@LOOP
+	1;JMP
+(END)
